d24d2c3562331acac1ba9fb231a4806ab4298ecc
Jahnvi Dhand <jahnvidhand@gmail.com>
2018-09-25 03:59:10 +0530

    lab2

---
+++ jahnvi lab2/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jahnvi_lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET : Products/Item
+        public ActionResult Item(String ItemName)
+
+        {
+            ViewBag.ItemName = ItemName;
+            return View();
+        }
+    }
+}
